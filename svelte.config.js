4de0bbc9402db7ca7da5b9a92690bdb695d56728
cdcd72 <cdcd71517@gmail.com>
2026-01-04 15:38:06 +0800

    feat: Switch SvelteKit adapter from static to auto, remove static fallback configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex, escapeSvelte } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
@@ -48,9 +48,7 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-            fallback: '404.html'
-        })
+        adapter: adapter()
     }
 };
 
